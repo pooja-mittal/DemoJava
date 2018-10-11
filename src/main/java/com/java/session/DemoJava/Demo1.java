8a591d66f683ba230f49d65cbc7758dd7ffb956f
Pooja <you@example.com>
2018-10-11 12:01:17 +0530

    auto merge

--- src/main/java/com/java/session/DemoJava/Demo1.java
+++ src/main/java/com/java/session/DemoJava/Demo1.java
@@ -4,7 +4,7 @@ public class Demo1 {
 
 	public static void main(String[] args) {
 		System.out.println("hello world");
-		System.out.println("hello beautiful what's going on ");
+		System.out.println("2nd line eclipse change");
 		System.out.println("added more line by git and continuess ");
 		System.out.println("pooja");		
 
