a68b7302809517b0f3baf04ed6b0aa86500249df
Pooja <you@example.com>
2018-10-11 11:36:08 +0530

    j

--- src/main/java/com/java/session/DemoJava/Demo1.java
+++ src/main/java/com/java/session/DemoJava/Demo1.java
@@ -5,9 +5,11 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello beautiful what's going on ");
-		System.out.println("this line added by git ");
+
 		System.out.println("added more line by git and continuess ");
-			
+
+		System.out.println("this line added by git and now i added by eclipse ");
+
 			
 
 	}
