2ae5fc0f7dcd3896d69eaaa4860c93de4744e230
Pooja <you@example.com>
2018-10-11 12:00:08 +0530

    ggh

--- src/main/java/com/java/session/DemoJava/Demo1.java
+++ src/main/java/com/java/session/DemoJava/Demo1.java
@@ -5,15 +5,8 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello beautiful what's going on ");
-
 		System.out.println("added more line by git and continuess ");
-
-
-		System.out.println("pooja");
-
-
-
-			
+		System.out.println("pooja");		
 
 	}
 
